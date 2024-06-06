4ddae1149c4ac32db10a6476bf9c1c6efac9124b
shokhrukh <114140248+Mrshokhrukh@users.noreply.github.com>
2024-06-06 11:01:03 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+type AppProps = {};
+
+const App: React.FC<AppProps> = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+};
+export default App;
