10fbe50de5b0f3a77452b8453fbbd5a3939eb850
Mrshokhrukh <114140248+Mrshokhrukh@users.noreply.github.com>
2024-06-24 00:40:49 +0500

    push 5

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -6,8 +6,12 @@ interface Props {
 
 const Card = ({ product }: Props) => {
   return (
-    <div className="w-full h-[320px] rounded-xl shadow-cardshadow cursor-pointer">
-      {}
+    <div className="h-[320px] hover:scale-105 p-3 transition duration-200 bg-white w-full rounded-xl shadow-cardshadow cursor-pointer">
+      <img src={product.img} alt="" />
+      <div className="px-1 mt-2 space-y-1">
+        <h2 className="font-bold capitalize text-lg">{product.name}</h2>
+        <p className="text-sm font-light">{product.desc}</p>
+      </div>
     </div>
   );
 };
--- src/Components/Products.tsx
+++ src/Components/Products.tsx
@@ -4,7 +4,7 @@ import Card from "./Card";
 const Products = () => {
   return (
     <div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
         {products.map((product) => {
           return <Card product={product} />;
         })}
