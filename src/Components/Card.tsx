d62684c1b3121e448163dd7cd71a7a1d435a5747
Mrshokhrukh <114140248+Mrshokhrukh@users.noreply.github.com>
2024-06-27 16:42:46 +0500

    card is ready

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -6,11 +6,21 @@ interface Props {
 
 const Card = ({ product }: Props) => {
   return (
-    <div className="h-[320px] hover:scale-105 p-3 transition duration-200 bg-white w-full rounded-xl shadow-cardshadow cursor-pointer">
+    <div className="h-[auto] hover:scale-105 p-3 transition duration-200 bg-white w-full rounded-xl shadow-cardshadow cursor-pointer">
       <img src={product.img} alt="" />
       <div className="px-1 mt-2 space-y-1">
         <h2 className="font-bold capitalize text-lg">{product.name}</h2>
         <p className="text-sm font-light">{product.desc}</p>
+        <div className="flex relative">
+          <p className="absolute text-lightgray font-light">{product.discountPrice}</p>
+          <img
+            className="absolute w-[65px] rotate-[14deg] left-[-4px] top-1"
+            src="https://popmenucloud.com/cdn-cgi/image/width=1920,height=1920,format=auto,fit=scale-down/hkxsgdpn/57f975b4-4271-4549-ad2f-dc82295abe52.png"
+            alt=""
+          />
+          <div className="text-sm font-semibold text-white ml-16 px-3 py-[3px] rounded-full bg-cartbtnred">{product.discountPercent}%</div>
+        </div>
+        <h1 className="font-semibold text-md">{product.price} so'm</h1>
       </div>
     </div>
   );
