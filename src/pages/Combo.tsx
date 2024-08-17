ef03583f405c22c4c40e3e13df19b18ac0b4cb26
Mrshokhrukh <shokhrukhabduvokhidov@gmail.com>
2024-08-18 01:07:09 +0500

    ready

--- src/pages/Combo.tsx
+++ src/pages/Combo.tsx
@@ -1,43 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { products } from "../db/data";
 // import { MdKeyboardArrowRight } from "react-icons/md";
 import ComboOptionSelection from "../Components/ComboOptionSelection";
 import ComboOptionModal from "../Components/ComboOptionModal";
 import ComboPriceAddToCart from "../Components/ComboPriceAddToCart";
+import { useQuery } from "react-query";
+import axios from "axios";
 
 type ComboProps = {};
 
 const Combo: React.FC<ComboProps> = () => {
   let { comboId } = useParams<{ comboId: string }>();
-  const [productData, setProductData] = useState<any>();
   const [comboFoodModalOpen, setComboFoodModalOpen] = useState<boolean>(false);
 
-  useEffect(() => {
-    const catchData = () => {
-      let data = products?.find((val) => String(val.id) === comboId);
-      setProductData(data);
-    };
+  const { data, isLoading } = useQuery("combo-details", () => {
+    return axios.get(`https://bellissimo-avt2.onrender.com/get_one_product/${comboId}`);
+  });
 
-    catchData();
-  }, []);
+  if (isLoading) {
+    return <h1>loading</h1>;
+  }
 
   return (
     <div className="flex gap-10 max-w-xxl mt-5 justify-around items-center mx-auto">
-      <img src={productData?.img} alt="" className="" />
+      <img src={data?.data.image} alt="" className="" />
       <div className="flex flex-col gap-2 w-[100%] max-w-[450px] pb-10">
-        <ComboOptionModal
-          ComboFoodModalOpen={comboFoodModalOpen}
-          setComboModalOpen={setComboFoodModalOpen}
-        />
+        <ComboOptionModal ComboFoodModalOpen={comboFoodModalOpen} setComboModalOpen={setComboFoodModalOpen} />
         <ComboOptionSelection setComboModalOpen={setComboFoodModalOpen} />
         <ComboOptionSelection setComboModalOpen={setComboFoodModalOpen} />
 
         <ComboOptionSelection setComboModalOpen={setComboFoodModalOpen} />
-        <ComboPriceAddToCart
-          productPrice={productData?.price}
-          dicountPrice={productData?.discountPrice}
-        />
+        <ComboPriceAddToCart productPrice={data?.data?.new_price} dicountPrice={data?.data?.old_price} />
       </div>
     </div>
   );
