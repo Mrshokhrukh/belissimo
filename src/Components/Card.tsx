18a940b30e1dfdcbd1e6fa6269e78b822dcbd6f2
Mrshokhrukh <shokhrukhabduvokhidov@gmail.com>
2024-08-18 19:46:51 +0500

    calculate dicount

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { products } from "../db/data";
-import { addProduct, calculateSum } from "../redux/CartSlice";
+import { addProduct } from "../redux/CartSlice";
 import { useDispatch } from "react-redux";
 import FormatPrice from "./formatPrice";
 
@@ -17,9 +17,17 @@ const Card = ({ product }: Props) => {
     if (data?.category === "combo") navigate(`/combo/${id}`);
     else {
       dispatch(addProduct(id));
-      dispatch(calculateSum());
     }
     localStorage.setItem("data-product", JSON.stringify(id));
+  };
+
+  const calculateDiscount = (newprice: number, oldprice: number) => {
+    if (newprice < oldprice) {
+      let calculatePercent = (newprice / oldprice) * 100;
+      return Math.floor(100 - calculatePercent);
+    } else {
+      return 0;
+    }
   };
 
   return (
@@ -43,7 +51,9 @@ const Card = ({ product }: Props) => {
               src="https://popmenucloud.com/cdn-cgi/image/width=1920,height=1920,format=auto,fit=scale-down/hkxsgdpn/57f975b4-4271-4549-ad2f-dc82295abe52.png"
               alt=""
             />
-            <div className="text-sm font-semibold text-white ml-16 px-3 py-[3px] rounded-full bg-cartbtnred">{10}%</div>
+            <div className="text-sm font-semibold text-white ml-16 px-3 py-[3px] rounded-full bg-cartbtnred">
+              {calculateDiscount(product.new_price, product.old_price)}%
+            </div>
           </div>
         )}
         <h1 className="font-semibold text-md">
